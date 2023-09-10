667da9f6fa7e03a2586f893e998c62dad5ddf09b
Ditzzy <mukhamadnurshodiqarridho_1701617092@mhs.unj.ac.id>
2023-09-10 18:01:50 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Start the desired command
+/bin/sh -c "cloudflared tunnel --url tcp://localhost:22 --url http://localhost:8080 --hostname trycloudflare.com & /kali.sh & gotty -p 8080 -w /bin/bash"
