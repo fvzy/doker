9ed39b62ccc920eedb2e0c204764755fdd884e36
Ditzzy <mukhamadnurshodiqarridho_1701617092@mhs.unj.ac.id>
2023-09-10 20:46:20 +0700

    NOPIENCE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Start the desired command
-/bin/sh -c "cloudflared tunnel --url tcp://localhost:22 --url http://localhost:8080 --hostname trycloudflare.com & /kali.sh & gotty -p 8080 -w /bin/bash"
+/bin/sh -c "cloudflared tunnel --url tcp://localhost:22 --url http://localhost:8080 --hostname trycloudflare.com & /kali.sh & x11vnc -forever -usepw -create & gotty -p 8080 -w /bin/bash"
