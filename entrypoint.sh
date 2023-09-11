e38db0886ef1d02df244969705edff28504a9a90
Ditzzy <mukhamadnurshodiqarridho_1701617092@mhs.unj.ac.id>
2023-09-11 22:10:09 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Start the desired command
-/bin/sh -c "cloudflared tunnel --url  --url tcp://localhost:5901 --hostname trycloudflare.com & /kali.sh & x11vnc -forever -usepw -create & gotty -p 8080 -w /bin/bash"
+/bin/sh -c "cloudflared tunnel --url tcp://localhost:8080 --hostname trycloudflare.com & /kali.sh & x11vnc -forever -usepw -create & gotty -p 8080 -w /bin/bash"
