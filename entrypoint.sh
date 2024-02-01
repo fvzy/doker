ad228c6487671c7f90aed5341b4258c2099cd5b3
Ditzzy <mukhamadnurshodiqarridho_1701617092@mhs.unj.ac.id>
2024-02-01 19:13:55 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-set -xe
-# Start the desired command
-/bin/sh /kali.sh &
-gotty -p 8080 -w /bin/bash &
-cloudflared tunnel --url http://localhost:8080 --hostname trycloudflare.com & 
-
+sh -c '
+    /bin/sh /kali.sh &
+    gotty -p 8080 -w /bin/bash &
+    cloudflared tunnel --url http://localhost:8080 --hostname trycloudflare.com
+' &
